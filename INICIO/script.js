e130f86c02599a1c2b9b88a5de6c3018f3053f89
Viciuss <viniupgames456@gmail.com>
2025-10-18 17:55:53 -0300

    Criação do front-end + idealização inicial do site

---
+++ INICIO/script.js
@@ -0,0 +1,10 @@
+const input = document.getElementById("campanario")
+const ouvir = document.getElementById("ouvir")
+
+input.addEventListener("click",() => {
+    window.location.href="../INPUT/"
+})
+
+ouvir.addEventListener("click",()=>{
+    window.location.href = "../OUTPUT/"
+})
---
+++ OUTPUT/script.js
@@ -0,0 +1,5 @@
+const voltar = document.getElementById("voltar")
+
+voltar.addEventListener("click", ()=>{
+    window.history.back()
+})
